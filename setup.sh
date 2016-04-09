0e1ab49783192f4d5a9b16cdab0fef2542615435
Jiří Altman <altman.jiri@gmail.com>
2016-04-10 00:13:24 +0200

    This fixes #2 and fixes #3

--- setup.sh
+++ setup.sh
@@ -10,25 +10,25 @@
 
 function _mw_apache
 {
-   /applications/apache/scripts/$1_apache.pl
+   "/applications/apache/scripts/$1_apache.pl"
 }
 alias mw_apache='_mw_apache'
 
 function _mw_tomcat
 {
-   /applications/tomcat/scripts/$1_tomcat.pl
+   "/applications/tomcat/scripts/$1_tomcat.pl"
 }
 alias mw_tomcat='_mw_tomcat'
 
 function _mw_websphere
 {
-   /applications/WebSphere/scripts/$1_websphere.pl
+   "/applications/WebSphere/scripts/$1_websphere.pl"
 }
 alias mw_websphere='_mw_websphere'
 
 function _mw_wls
 {
-   /applications/WLS/scripts/$1_WLS.pl
+   "/applications/WLS/scripts/$1_WLS.pl"
 }
 alias mw_websphere='_mw_wls'
 
